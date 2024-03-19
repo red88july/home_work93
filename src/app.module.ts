7ab891d3ab48a153224b4aba922f56ec39e5bb13
Sergeev Mikhail <sergeev-michael@mail.ru>
2024-03-19 16:09:47 +0600

    Create three controllers artsts, albums, tracks. Create schemas dir and artist.schema.ts"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArtistsController } from './artists/artists.controller';
+import { AlbumsController } from './albums/albums.controller';
+import { TracksController } from './tracks/tracks.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ArtistsController, AlbumsController, TracksController],
   providers: [AppService],
 })
 export class AppModule {}
