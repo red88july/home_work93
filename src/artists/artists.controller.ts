17b9b2392f4f2728fbe49dd300befea96bdae48b
Sergeev Mikhail <sergeev-michael@mail.ru>
2024-03-20 11:51:55 +0600

    Create logic for DELETE request for artist(add check for if artist by id not found and add check for success deleted)

--- src/artists/artists.controller.ts
+++ src/artists/artists.controller.ts
@@ -1,12 +1,24 @@
-import {Body, Controller, Get, Post, Req, UploadedFile, UploadedFiles, UseInterceptors} from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get, HttpException, HttpStatus, Next, NotFoundException,
+    Param,
+    Post,
+    Req, Res,
+    UploadedFile,
+    UploadedFiles,
+    UseInterceptors
+} from '@nestjs/common';
 import {InjectModel} from "@nestjs/mongoose";
 import {Artist, ArtistDocument} from "../schemas/artist.schema";
 import {Model} from "mongoose";
 import {CreateUserDto} from "./create-user.dto";
-import { FileInterceptor } from "@nestjs/platform-express";
-import { diskStorage } from "multer";
-import { extname } from 'path';
-import { randomUUID } from "crypto";
+import {FileInterceptor} from "@nestjs/platform-express";
+import {diskStorage} from "multer";
+import {extname} from 'path';
+import {randomUUID} from "crypto";
+import {onErrorResumeNext} from "rxjs";
 
 const storage = diskStorage({
     destination: './public/uploads/artist',
@@ -20,7 +32,9 @@ const storage = diskStorage({
 
 @Controller('artists')
 export class ArtistsController {
-    constructor(@InjectModel(Artist.name) private artistModel: Model<ArtistDocument>) {}
+    constructor(@InjectModel(Artist.name) private artistModel: Model<ArtistDocument>) {
+    }
+
     @Post()
     @UseInterceptors(
         FileInterceptor(
@@ -32,11 +46,14 @@ export class ArtistsController {
         @UploadedFile() file: Express.Multer.File,
         @Body() usersDto: CreateUserDto
     ) {
+
+
         const user = new this.artistModel({
             author: usersDto.author,
             info: usersDto.info,
-            photo: file ? '/uploads/artists' + file.filename : null,
+            photo: file ? '/uploads/artists/' + file.filename : null,
         })
+
 
         return user.save();
     }
@@ -44,5 +61,29 @@ export class ArtistsController {
     @Get()
     getAll() {
         return this.artistModel.find();
+    }
+
+    @Get(':id')
+    getOneArtist(@Param('id') id: string) {
+        return this.artistModel.findById(id);
+    }
+
+    @Delete('delete/:id')
+    async getByIdAndDelete(
+        @Res() res: any,
+        @Param('id') id: string
+    ) {
+        const artist = await this.artistModel.findById(id);
+
+        if (!artist) {
+            throw new NotFoundException('User not found');
+        }
+
+        const deletedArtist = await this.artistModel.findByIdAndDelete(id)
+        return res.status(HttpStatus.OK)
+            .json({
+                message: 'User was been deleted',
+                artist: deletedArtist
+            });
     }
 }
